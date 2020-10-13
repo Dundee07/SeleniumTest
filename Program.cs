97fb350f9c57b63f37f0d879206d4d51b9bf2c38
Dundee07 <72821779+Dundee07@users.noreply.github.com>
2020-10-13 22:19:36 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+            
+            driver.Navigate().GoToUrl("https://s1.demo.opensourcecms.com/wordpress/wp-login.php/");
+            driver.Manage().Window.Maximize();
+
+            
+            driver.FindElement(By.Name("log")).SendKeys("opensourcecms");
+            driver.FindElement(By.Name("pwd")).SendKeys("opensourcecms"+ Keys.Enter);
+
+            driver.FindElement(By.XPath("/html/body/div[1]/div[1]/div[2]/ul/li[3]/a/div[3]")).Click();
+            driver.FindElement(By.XPath("/html/body/div[1]/div[1]/div[2]/ul/li[3]/ul/li[5]/a")).Click();
+
+            driver.FindElement(By.Id("tag-name")).SendKeys("1234"+ Keys.Enter);
+        }
+    }
+}
