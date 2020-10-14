60396dc76bc4d2d84c2056c0743aed57fbab4bef
Dundee07 <72821779+Dundee07@users.noreply.github.com>
2020-10-14 10:57:41 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using OpenQA.Selenium;
+using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System;
 using System.Collections.Generic;
@@ -14,15 +14,22 @@ namespace SeleniumTest
         {
             IWebDriver driver = new ChromeDriver();
             
-            driver.Navigate().GoToUrl("https://s1.demo.opensourcecms.com/wordpress/wp-login.php/");
+            driver.Navigate().GoToUrl("https://s1.demo.opensourcecms.com/wordpress/wp-login.php");
+            //driver.Navigate().GoToUrl("https://www.opensourcecms.com/wordpress/");
             driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
 
-            
+            //driver.FindElement(By.XPath("/html/body/div[2]/div[2]/div[1]/div[1]/div[2]/div[3]/ul/li[2]/a")).Click();
+
+
             driver.FindElement(By.Name("log")).SendKeys("opensourcecms");
             driver.FindElement(By.Name("pwd")).SendKeys("opensourcecms"+ Keys.Enter);
 
             driver.FindElement(By.XPath("/html/body/div[1]/div[1]/div[2]/ul/li[3]/a/div[3]")).Click();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(2);
+
             driver.FindElement(By.XPath("/html/body/div[1]/div[1]/div[2]/ul/li[3]/ul/li[5]/a")).Click();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(2);
 
             driver.FindElement(By.Id("tag-name")).SendKeys("1234"+ Keys.Enter);
         }
